1a79270131127a5fbf395b9e9fbf14eb129135fb
Neha Singh <839604@maruti.co.in>
2025-04-09 12:52:52 +0530

    Committing changes from dev branch. Made changes to HomeController class.

--- GitDemo/Controllers/HomeController.cs
+++ GitDemo/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace GitDemo.Controllers
 {
@@ -15,14 +11,14 @@ namespace GitDemo.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Application description page";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact page";
 
             return View();
         }
